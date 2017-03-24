b86c32a6defa62d39b55fda420cec98df360829f
Diamantis Sellis <sellisd@gmail.com>
2017-03-24 16:37:47 +0100

    Add example and fix variable names.

--- sharedFunctions.R
+++ sharedFunctions.R
@@ -8,17 +8,19 @@ library(seqinr) #s2c
 #' @param side    size of square
 #' @param type    ["string"|"vector"] of integers
 #'
-#' @return
+#' @return side x side numeric matrix
 #' @export
 #'
-#' @examples
+#' @examples:
+#' a <- read.table("~/projects/GoL/caNI100.v.dat", nrows = 100)
+#' image(vector2matrix(v=as.vector(unlist(a[14,c(2:length(a))])), side = 64,type = "vector"))
 vector2matrix <- function(vectorS, type, side){
   if (type == "string"){
-    v <- as.numeric(s2c(vectorS))
+    vectorS <- as.numeric(s2c(vectorS))
   }else if (type == "vector"){
     # pass
   }else{
     warning(paste("Unknown type", type))
   }
-  matrix(v, nrow = side, byrow = TRUE)
+  matrix(vectorS, nrow = side, byrow = TRUE)
 }
